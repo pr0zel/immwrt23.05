41a2acf8c6ca18244e5b9dc5eb22c549f239bf10
Sakana <pr0zel@gmail.com>
2023-11-09 18:54:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,6 @@
 
 sed -i 's/CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND=y/CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL=y/g' target/linux/x86/64/config-5.15
 sed -i 's/# CONFIG_CPU_FREQ_GOV_CONSERVATIVE is not set/CONFIG_CPU_FREQ_GOV_CONSERVATIVE=y/g' target/linux/x86/64/config-5.15
+
+sed -i 's/PKG_VERSION:=8.051.02/PKG_VERSION:=8.052.01/g' package/kernel/r8168/Makefile
+rm ./package/kernel/r8168/patches/001-r8168-add-LED-configuration-from-OF.patch
